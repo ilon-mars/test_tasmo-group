e4eb010e661540d8b00b89954290bbd888c21dbd
Maryana <ilon.mars@yandex.ru>
2022-03-04 02:58:58 +0300

    add: vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
+import { store } from '@/store';
 import App from './App.vue';
 
 import '@/assets/sass/main.sass';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(store).mount('#app');
