3e3e4b09e2007cf05e99f6376779ac1099e84432
Maryana <ilon.mars@yandex.ru>
2022-03-04 12:22:05 +0300

    add: dist golder to gh-pages

--- src/components/MessageStatusSuccess.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <span class="message__status">
-    <svg class="message__status-icon status status--success">
-      <use xlink:href="@/assets/img/sprite.svg#success" />
-    </svg>
-  </span>
-</template>
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import { store } from '@/store';
-import App from './App.vue';
-
-import '@/assets/sass/main.sass';
-
-const app = createApp(App);
-
-app.use(store).mount('#app');
